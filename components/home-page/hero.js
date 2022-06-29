0bf8b5a99ea12b1dca17abca2e7ac68dc7c5dced
Ryan <ryanhin@hotmail.com>
2022-06-29 20:34:54 +0800

    commit

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -9,9 +9,9 @@ function Hero() {
                 <Image src='/images/site/JacobJessie.jpg' alt='Cute Photo' width={300} height={300} />
             </div>
             <motion.div
-                initial={{ x: -300 }}
+                initial={{ x: -350 }}
                 animate={{ x: 0 }}
-                transition={{ ease: "easeOut", duration: 0.6 }}>
+                transition={{ ease: "easeOut", duration: 0.7 }}>
                 <h1>Jacob and Jessie</h1>
             </motion.div>
             <p>We are cute little dogs writing a blog :D</p>
