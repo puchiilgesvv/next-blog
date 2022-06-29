00dacfa7f480c81531cdb8eb992966afb6180dc8
Ryan <ryanhin@hotmail.com>
2022-06-29 16:58:05 +0800

    commit

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,7 +7,7 @@ function Hero() {
             <div className={classes.image}>
                 <Image src='/images/site/JacobJessie.jpg' alt='Cute Photo' width={300} height={300} />
             </div>
-            <h1>Jacob Jessie Blog</h1>
+            <h1>Jacob and Jessie</h1>
             <p>We are cute little dogs writing a blog :D</p>
         </section>
     )
--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,6 +1,8 @@
+import classes from "./logo.module.css"
+
 function Logo() {
     return (
-        <p>Home</p>
+        <div className={classes.logo}>Jacob Jessie Blog</div>
     )
 }
 
