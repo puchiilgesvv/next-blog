4c66d90b941a8fb3e503d0b2c62b461901fc9118
Ryan <ryanhin@hotmail.com>
2022-07-12 17:10:30 +0800

    Index js now reads the markdown files

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,48 @@
 import Animate from "../components/animations/animate"
 import FeaturedPosts from "../components/home-page/featured-posts"
 import Hero from "../components/home-page/hero"
+import { getFeaturedPosts } from "../lib/posts-util"
 
-export default function Home() {
-  const DUMMY_POSTS = [
-    {
-      slug: "getting-started-with-next-js",
-      title: "Getting Started with Next JS",
-      image: "getting-started-nextjs.png",
-      excerpt: "Next JS is the React framework for production.",
-      date: "2022-02-10"
-    },
-    {
-      slug: "a-random-post",
-      title: "This is a random post",
-      image: "getting-started-nextjs.png",
-      excerpt: "Next JS is amazing",
-      date: "2022-05-10"
-    },
-    {
-      slug: "another-random-post",
-      title: "This is another random post",
-      image: "getting-started-nextjs.png",
-      excerpt: "Next JS is ultra amazing!!!",
-      date: "2022-05-10"
-    }
-  ]
+export default function Home(props) {
+  // const DUMMY_POSTS = [
+  //   {
+  //     slug: "getting-started-with-next-js",
+  //     title: "Getting Started with Next JS",
+  //     image: "getting-started-nextjs.png",
+  //     excerpt: "Next JS is the React framework for production.",
+  //     date: "2022-02-10"
+  //   },
+  //   {
+  //     slug: "a-random-post",
+  //     title: "This is a random post",
+  //     image: "getting-started-nextjs.png",
+  //     excerpt: "Next JS is amazing",
+  //     date: "2022-05-10"
+  //   },
+  //   {
+  //     slug: "another-random-post",
+  //     title: "This is another random post",
+  //     image: "getting-started-nextjs.png",
+  //     excerpt: "Next JS is ultra amazing!!!",
+  //     date: "2022-05-10"
+  //   }
+  // ]
 
   return (
     <Animate>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Animate>
   )
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts()
+  
+  return {
+    props: {
+      posts:featuredPosts
+    },
+    revalidate: 6000
+  }
 }
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,11 @@
+---
+title: 'This is some post title'
+date: '2022-7-12'
+image: getting-started-nextjs.png
+excerpt: 'Some more dummy content'
+isFeatured: true
+---
+
+# This is some title
+
+This is some text.
