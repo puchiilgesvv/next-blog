efa5cc68c951181ab6998c7683a0d167f9c136de
Ryan <ryanhin@hotmail.com>
2022-07-14 10:27:12 +0800

    Added DB functionality api

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,25 +1,103 @@
+import { useEffect, useState } from "react"
 import Animate from "../animations/animate"
+import Notification from "../ui/notification"
 import classes from "./contact-form.module.css"
 
 export default function ContactForm() {
+    const [enteredEmail, setEnteredEmail] = useState('')
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredMessage, setEnteredMessage] = useState('')
+    const [requestStatus, setRequestStatus] = useState()
+    const [requestError, setRequestError] = useState()
+
+    useEffect(() => {
+        if (requestStatus === 'success' || requestStatus === 'error') {
+            const timer = setTimeout(() => {
+                setRequestStatus(null)
+                setRequestError(null)
+            }, 3000)
+
+            return () => clearTimeout(timer)
+        }
+    }, [requestStatus])
+
+    async function sendMessageHandler(event) {
+        event.preventDefault()
+        setRequestStatus('pending')
+
+        let response
+        try {
+            response = await fetch('/api/contact', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: enteredEmail,
+                    name: enteredName,
+                    message: enteredMessage
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            setRequestStatus('success')
+            setEnteredEmail('')
+            setEnteredName('')
+            setEnteredMessage('')
+        } catch (error) {
+            setRequestError(error.message)
+            setRequestStatus('error')
+        }
+
+
+        const data = await response.json()
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong.')
+        }
+    }
+
+    let notification
+
+    if (requestStatus === 'pending') {
+        notification = {
+            status: 'pending',
+            title: 'Sending message...',
+            message: 'Your message is on its way'
+        }
+    }
+
+    if (requestStatus === 'success') {
+        notification = {
+            status: 'success',
+            title: 'Success',
+            message: 'Your message was sent successfully'
+        }
+    }
+
+    if (requestStatus === 'error') {
+        notification = {
+            status: 'error',
+            title: 'Failed',
+            message: requestError
+        }
+    }
+
     return (
         <Animate>
             <section className={classes.contact}>
                 <h1>How can I help you?</h1>
-                <form className={classes.form}>
+                <form className={classes.form} onSubmit={sendMessageHandler}>
                     <div className={classes.controls}>
                         <div className={classes.control}>
                             <label htmlFor="email">Your Email</label>
-                            <input type='email' id='email' required></input>
+                            <input type='email' id='email' required value={enteredEmail} onChange={event => setEnteredEmail(event.target.value)}></input>
                         </div>
                         <div className={classes.control}>
                             <label htmlFor="name">Your Name</label>
-                            <input type='text' id='name' autoComplete="off" required></input>
+                            <input type='text' id='name' autoComplete="off" required value={enteredName} onChange={event => setEnteredName(event.target.value)}></input>
                         </div>
                     </div>
                     <div className={classes.control}>
-                        <label htmlFor="message">Your Message</label>
-                        <textarea rows='5'></textarea>
+                        <label htmlFor="message" >Your Message</label>
+                        <textarea rows='5' value={enteredMessage} onChange={event => setEnteredMessage(event.target.value)}></textarea>
                     </div>
                     <div className={classes.actions}>
                         <button>
@@ -27,8 +105,8 @@ export default function ContactForm() {
                         </button>
                     </div>
                 </form>
+                {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
             </section>
         </Animate>
-
     )
 }
---
+++ pages/api/contact.js
@@ -0,0 +1,41 @@
+import { connectDatabase } from "../../lib/db-util"
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { email, name, message } = req.body
+
+        if (!email || !email.includes('@') || !name || name.trim() === '' || !message || message.trim() === '') {
+            res.status(422).json({ message: 'Invalid form submission!' })
+            return
+        }
+
+        const newMessage = {
+            email,
+            name,
+            message
+        }
+
+        let client
+
+        try {
+            client = await connectDatabase()
+        } catch (error) {
+            res.status(500).json({ message: error.message ? error.message : 'Failed to connect to database.' })
+            return
+        }
+
+        const db = client.db('blog')
+        try {
+            const result = await db.collection('form').insertOne(newMessage)
+            newMessage.id = result.insertedId
+        } catch (error) {
+            client.close()
+            res.status(500).json({ message: 'Somehow it failed.' })
+            return
+        }
+
+        client.close()
+
+        res.status(201).json({ message: 'Successfully sent!', form: newMessage })
+    }
+}
